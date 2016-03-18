c1dfc301124d67c6e7d876e9fd80c5bda7b22606
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-03-18 12:18:45 -0700

    Completed Mouse-a-sketch exercise

--- mouse_a_sketch/base.js
+++ mouse_a_sketch/base.js
@@ -1,5 +1,10 @@
 console.log("this is a sanity check");
 
 $(document).ready(function(){
-  console.log("Let's get coding!")
-})
+  console.log("Let's get coding!");
+
+  $("td").on("mouseenter", function() {
+      $(this).css("background-color",$("input").val());
+      console.log($("input").val());
+  });
+});
